b1040b3dd46afb93c2366b1c641ae201e69259ca
Rubenhdz20 <rubhernandez42@gmail.com>
2024-09-24 13:49:35 -0600

    learn arrays in ts

--- src/07-array.ts
+++ src/07-array.ts
@@ -1,1 +1,15 @@
-()
+(() => {
+  let prices = [1, 2, 3, 4, 5, 6];
+  // prices.push("numbers");
+  // prices.push(true);
+  // prices.push({});
+  prices.push(420);
+
+  let products = ["hola", true];
+  products.push(false);
+
+  let mixed: (string | number | boolean | object)[] = ["canelo"];
+
+  let numbers = [1, 2, 3, 4, 5, 6];
+  numbers.map(item => item * 2);
+})();
